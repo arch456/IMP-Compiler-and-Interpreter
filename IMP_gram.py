aa99d89f654247b2681c8fd7c24157406293d0d5
Archana Chittoor <arcanenarchu@gmail.com>
2019-12-14 18:46:48 -0500

    Updated parser and lexer files and tested them successfully

---
+++ IMP_gram.py
@@ -0,0 +1,71 @@
+# Grammar for IMP language
+
+from ply import yacc
+from IMP_lex import tokens, lexer
+
+# define the precedence and associativity for the language here
+precedence = (('left', 'AND', 'OR'),
+              ('left', 'EQ', 'NE', 'LT', 'GT', 'LE', 'GE'),
+              ('left', 'PLUS', 'MINUS'),
+              ('left', 'TIMES', 'DIVIDE'),
+              ('right', 'UMINUS', 'NOT')
+             )
+
+# define the grammar for IMP language
+def p_grammar(_):
+    '''
+    program : stmt
+
+    stmt : block
+         | ID ASSIGN aexp ';'
+         | stmt stmt  
+         | GET ID ';'
+         | PUT exp ';'      
+         | IF '(' bexp ')' THEN block opt_else END
+         | WHILE '(' bexp ')' DO block END
+
+    opt_else : ELSE block
+             | empty
+             
+    block : '{' '}'
+          | '{' stmt '}'
+          
+    exp : aexp
+        | bexp
+        
+    bexp : BOOL
+         | aexp EQ aexp
+         | aexp NE aexp
+         | aexp LT aexp
+         | aexp GT aexp
+         | aexp LE aexp
+         | aexp GE aexp
+         | NOT bexp
+         | bexp AND bexp
+         | bexp OR bexp
+         
+    aexp : INT
+         | ID
+         | aexp PLUS aexp
+         | aexp MINUS aexp
+         | aexp TIMES aexp
+         | aexp DIVIDE aexp
+         | MINUS exp %prec UMINUS
+
+    '''
+    pass
+
+def p_empty(p):
+    'empty :'
+    pass
+
+def p_error(t):
+    print("Syntax error at '%s'" % t.value)
+
+### build the parser
+parser = yacc.yacc()
+
+
+
+
+ 
---
+++ IMP_lex.py
@@ -0,0 +1,84 @@
+# Lexer for IMP language
+
+from ply import lex
+
+reserved = {
+    'get'   : 'GET',
+    'put'   : 'PUT',
+    'if'    : 'IF',
+    'else'  : 'ELSE',
+    'while' : 'WHILE',
+    'then'  : 'THEN',
+    'not'   : 'NOT',
+    'end'   : 'END' ,
+    'do'    : 'DO'
+}
+
+literals = [';',':','=','(',')','{','}']
+
+tokens = [
+          'PLUS','MINUS','TIMES','DIVIDE',
+          'EQ','NE','LT','GT','LE','GE','AND','OR', 
+          'INT','ID','BOOL','ASSIGN'
+          ] + list(reserved.values())
+
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_EQ      = r'=='
+t_NE      = r'!='
+t_LT      = r'<'
+t_GT      = r'>'
+t_LE      = r'<='
+t_GE      = r'>='
+t_AND     = r'&&'
+t_OR      = r'\|\|'
+t_ASSIGN  = r':='
+
+t_ignore = ' \t'
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'ID')    # Check for reserved words
+    return t
+
+def t_INT(t):
+    r'[0-9]+'
+    return t
+
+def t_BOOL(t):
+    r'[0|1]'
+    return t
+
+def t_COMMENT(t):
+    r'\#.*'
+    pass
+
+def t_NEWLINE(t):
+    r'\n'
+    pass
+
+def t_error(t):
+    print("Illegal character %s" % t.value[0])
+    t.lexer.skip(1)
+
+# build the lexer
+lexer = lex.lex(debug=0)
+
+
+
+
+
+
+
+       
+   
+  
+            
+
+
+
+
+
+
